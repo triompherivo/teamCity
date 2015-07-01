718551a281914cd901e0f086019664b50849c380
triompherivo <triompherivo@gmail.com>
2015-07-01 18:01:46 +0200

    add modify and publish

---
+++ TeamCityProject/localRun.ps1
@@ -0,0 +1,7 @@
+﻿$proj = ".\TeamCityProject.csproj"
+
+$azure = ".\Properties\PublishProfiles\prof.pubxml"
+
+$pwd = "mGGNoahDsMvhjbJSrwyW5in5bQsZEodkDnnd2PpEAnS1jx6hjurLYDJPJxrA"
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe $proj /p:DeployOnBuild=true /p:PublishProfile=Production /p:PublishProfile=$azure /p:VisualStudioVersion=11.0 /p:Password=$pwd /p:AllowUntrustedCertificate=true
