1d55359e7060fd3eb07af46a1f9d514f4cf122ad
triompherivo <triompherivo@gmail.com>
2015-07-01 16:24:13 +0200

    Add test

--- TeamCityProject.Tests/UnitTest1.cs
+++ TeamCityProject.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeamCityProject.Controllers;
 
 namespace TeamCityProject.Tests
 {
@@ -9,6 +11,14 @@ namespace TeamCityProject.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
         }
     }
 }
